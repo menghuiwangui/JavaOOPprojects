c4f8a1d6846a935bdc37eb82e0344c58a8d9e45e
superideol <2331786873@qq.com>
2026-04-27 14:17:02 +0800

    1.05

--- src/ProductHouse.java
+++ src/ProductHouse.java
@@ -8,19 +8,40 @@ public class ProductHouse {
         products = new HashMap<>();
     }
 
-    void addProduct(IProduct product)  // 添加商品
+    public boolean addProduct(IProduct product)  // 添加商品  ######
     {
-        products.put(product.getId(), product);
+        if (!products.containsKey(product.getId())){
+            System.out.println("添加商品成功!");
+            System.out.println("商品类别: " + product.getCategory() + " " + product.getId() + " " + product.getName() + " " + product.getPrice() + " " + product.getStock());
+            products.put(product.getId(), product);
+            return true;
+        }
+        else 
+        {
+            System.out.println("商品已存在!");
+             System.out.println("存在的商品类别: " + product.getCategory() + " " + product.getId() + " " + product.getName() + " " + product.getPrice() + " " + product.getStock());
+            return false;
+        }
     }
 
-    IProduct getProduct(String id)  // 按名字查询
+    public IProduct getProduct(String id)  // 按名字查询
     {
         return products.get(id);
     }
 
-    public void removeProduct(String id)  // 删除商品
+    public boolean removeProduct(String id)  // 删除商品  ######
     {
-        products.remove(id);
+        if (products.containsKey(id))
+        {
+            products.remove(id);
+            System.out.println("删除商品成功!");
+            return true;
+        }
+        else
+        {
+            System.out.println("商品不存在!");
+            return false;
+        }
     }
 
     public void readProducts() // 查看所有商品
