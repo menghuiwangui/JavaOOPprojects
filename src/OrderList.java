a7664758b9d6aba9d919570f8948d0a5541f9b7c
superideol <2331786873@qq.com>
2026-04-27 14:15:22 +0800

    1.04

---
+++ src/OrderList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class OrderList 
+{
+    private ArrayList<Order> orderList;
+
+    public OrderList() 
+    {
+
+    }
+
+    public void addOrder(Order order) 
+    {
+        orderList.add(order);
+    }
+
+    public void removeOrder(Order order) 
+    {
+
+    }
+}
