5bb62e135a58a790cb4fe407391c4d77cfad0608
menghuiwangui <2964668101@qq.com>
2026-05-09 01:06:13 +0800

    1.38
    fix:修复电商购物系统退出逻辑

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,8 @@ public class Main
     public static void exit_program(){
         scanner.close();//加入scanner已经关闭了再调用不会抛出异常
         System.out.println("简易电商购物车系统正在退出，感谢您的使用");
+        System.exit(0);
+
     }
     public static void mainMenu()  // 主菜单，暂无问题
     {
